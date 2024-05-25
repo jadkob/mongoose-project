9fc8cb1094797fd5686330f4a63c3f91805a002c
jad <jadkoneissi@gmail.com>
2024-05-25 22:53:11 +0300

    ali fix

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -2,8 +2,10 @@ import { User } from "../models/user";
 import { isEmpty } from "../isEmpty";
 import * as jwt from "jsonwebtoken";
 import * as bcrypt from "bcrypt";
+import { connectDb } from "../setup";
 
 export async function POST(req: Request) {
+  connectDb();
   try {
     const { username, password } = await req.json();
 
